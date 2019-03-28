0fd4cc28961632ec12045b867ef0b4e62ef307b5
harsha <hghanta@andrew.cmu.edu>
2019-03-28 14:26:18 -0400

    added producer consumer , buggy though

--- encrypt.c
+++ encrypt.c
@@ -20,28 +20,33 @@ struct tip {
     char *output;
     char *input;
     char *key;
-
-    long chunksize;
     long inputsize;
-    int thread_id;
-
 };
+
+
 
 typedef struct tip thread_input;
 
 pthread_cond_t cv;
 pthread_mutex_t m;
-int count;
+pthread_cond_t cv2;
+pthread_mutex_t m2;
+bool outputDone=false;
+bool inputDone=false;
+
+long  outputBufferfill=0;
+long currentBufferFill=0;
+long BUFFER_SIZE;
+long bytes_in_buffer;
 
 
-count = 0;
-
-bool fxor(FILE *in, FILE *out, FILE *key);
 void left_shift_key(uint8_t *existingKey, long size);
 void *getXorOutput(thread_input *threadinput);
 
 static const char *HELP_MESSAGE = " The usage encrypt -k keyfile.bin -n 1 < plain.bin > cypher.bin";
 const int CHAR_SIZE= sizeof(char);
+static const int BUFFER_CONSTANT =4;
+void outputData(char *outputbuffer);
 
 int main(int argc, char **argv) {
 
@@ -101,92 +106,119 @@ int main(int argc, char **argv) {
     */
 
 
-
-
   /*
    * Make the dummy input ready for testing
    */
 
 
-   int NUM_THREADS = 4;
-   FILE *inputfile = fopen("/Users/harsha/CLionProjects/encrypt/input","rb");
+   int NUM_THREADS = 10;
 
    FILE *keyfile = fopen("/Users/harsha/CLionProjects/encrypt/keyfile","rb");
    fseek(keyfile,0L,SEEK_END);
    long chunk_size = ftell(keyfile);
    rewind(keyfile);
+   BUFFER_SIZE = BUFFER_CONSTANT * NUM_THREADS * chunk_size;
 
 
-   unsigned  char keybuffer[chunk_size];
+    unsigned  char keybuffer[chunk_size];
    for(int i=0;i<chunk_size;i++){
        keybuffer[i] = fgetc(keyfile);
    }
 
 
-   char outputbuffer[NUM_THREADS*chunk_size];                // final output buffer that all threads output will be written to
-   void *status;
-   //printf("entering main loop\n");
-   int pos=0;                           //global buffer position
+   char outputbuffer[BUFFER_SIZE];   // final output buffer that all threads output will be written to
+   char inputbuffer[BUFFER_SIZE];
 
    //create a thread pool with NUM_THREADS
    threadpool_t *pool = threadpool_create(NUM_THREADS,4*NUM_THREADS,0);
+   int outwrite = open("/Users/harsha/CLionProjects/encrypt/output",O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-
-    int outwrite = open("/Users/harsha/CLionProjects/encrypt/output",O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+   //create a consumer thread and wait for the output buffer to get filled
+   pthread_t tid;
+   pthread_create(&tid,NULL,outputData,outputbuffer);
 
    while(!feof(stdin)) {
 
-       thread_input *inputs = malloc(sizeof(thread_input) * NUM_THREADS);
-        int spawned_threads=0;
+       bytes_in_buffer= fread(inputbuffer,CHAR_SIZE,BUFFER_SIZE,stdin);
+       if(bytes_in_buffer==0) {
+           inputDone=true;
+           break;
 
-        while(!feof(stdin) && spawned_threads < NUM_THREADS){
-           // printf("entering second main \n");
-            thread_input *tempStore = inputs+spawned_threads;
-            tempStore->input= malloc(sizeof(char) * chunk_size);
-            long bytesRead = fread(tempStore->input, CHAR_SIZE, chunk_size, stdin);
-            //printf("byts read %d\n", (int) bytesRead);
-            if(bytesRead == 0) { break;}
-            tempStore->inputsize = bytesRead;
+       }
+
+      currentBufferFill=0;
+       long processed_bytes=0;
+
+        while(processed_bytes < bytes_in_buffer){
+            thread_input *tempStore = malloc(sizeof(thread_input));
+            if(processed_bytes+chunk_size < bytes_in_buffer) {
+                tempStore->input = malloc(sizeof(char) * chunk_size);
+                tempStore->inputsize = chunk_size;
+                memcpy(tempStore->input,inputbuffer+processed_bytes,chunk_size);
+                processed_bytes +=chunk_size;
+                currentBufferFill=currentBufferFill+chunk_size;
+            }else{
+                long left_over = bytes_in_buffer-processed_bytes;
+                tempStore->input= malloc(sizeof(char) * (left_over));
+                tempStore->inputsize = left_over;
+                memcpy(tempStore->input,inputbuffer+processed_bytes,left_over);
+                processed_bytes+=left_over;
+                currentBufferFill=currentBufferFill+left_over;
+                inputDone=true;
+            }
             tempStore->key = malloc(sizeof(char) * chunk_size);
             memcpy(tempStore->key,keybuffer,chunk_size);
-
-            tempStore->output = malloc(sizeof(char)* bytesRead);
-            tempStore->thread_id=spawned_threads;
-            threadpool_add(pool, (void (*)(void *)) getXorOutput, tempStore, 0);
             left_shift_key(keybuffer, chunk_size);
-            spawned_threads++;
+            tempStore->output = outputbuffer+processed_bytes;
+            threadpool_add(pool, (void (*)(void *)) getXorOutput, tempStore, 0);
         }
 
-        //collect the data for that spawned threads so far
-       pthread_mutex_lock(&m);
-       while(count<spawned_threads){
-           pthread_cond_wait(&cv, &m);
+        //wait on signal from producer(which prints to screen)  that it has finished one iter size of data
+       pthread_mutex_lock(&m2);
+       while(outputDone != true){
+           pthread_cond_wait(&cv2, &m2);
        }
-       pthread_mutex_unlock(&m);
-
-       count=0;
-       int buffsize=0;
-       memset(outputbuffer,0, spawned_threads* chunk_size);
-       for(int  j=0;j<spawned_threads;j++){
-           memcpy(outputbuffer+buffsize,inputs[j].output,inputs[j].inputsize);
-           buffsize = buffsize+inputs[j].inputsize;
-       }
-       write(fileno(stdout),outputbuffer,buffsize);
-       free(inputs);
+       outputDone=false;
+       pthread_mutex_unlock(&m2);
 
    }
    close(outwrite);
-    return 0;
+   return 0;
 }
 
 void *getXorOutput(thread_input *threadinput){
      for(int i=0;i < (threadinput->inputsize) ; i++){
             threadinput->output[i] = threadinput->input[i] ^ (threadinput->key[i]);
      }
-    pthread_mutex_lock(&m);
-    count++;
-    pthread_cond_signal(&cv);
+     pthread_mutex_lock(&m);
+    outputBufferfill += threadinput->inputsize;
+    if(outputBufferfill == currentBufferFill) pthread_cond_signal(&cv);
     pthread_mutex_unlock(&m);
+    //act like a producer and add the output to corresponding buffer location
+}
+
+
+void outputData(char *outputbuffer){
+
+
+  while(!inputDone) {
+      pthread_mutex_lock(&m);
+      while (outputBufferfill < bytes_in_buffer) {
+          pthread_cond_wait(&cv, &m);
+      }
+
+      write(fileno(stdout), outputbuffer, outputBufferfill);
+      outputDone=true;
+      outputBufferfill = 0;
+
+      //signal the main thread to process to fetch/read data from stdin
+      pthread_mutex_lock(&m2);
+      pthread_cond_signal(&cv2);
+      pthread_mutex_unlock(&m2);
+
+      pthread_mutex_unlock(&m);
+  }
+
 }
 
 void left_shift_key(uint8_t *existingKey, long size){
