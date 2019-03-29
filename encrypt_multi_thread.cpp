5ee1a517062299738b156db090023b9f78eb417d
syshack <42792098+harshvg@users.noreply.github.com>
2019-03-29 13:39:17 +0530

    added mutex lock and some cleanup

--- encrypt_multi_thread.cpp
+++ encrypt_multi_thread.cpp
@@ -36,12 +36,8 @@ typedef struct _data_buf
 queue <data_buf_t*> input_queue;
 
 #define FILE_NAME_SIZE 256
-int MAX_QUEUE_SIZE ;
-int MAX_BUFFERS;
-uint32_t key_length;
-uint32_t current_token_id =0 ;
-uint32_t last_assigned_token_id =0;
-
+;
+pthread_mutex_t lock; 
 data_buf_t **arr_pointers ;
 
 void shift_1bits_left(uint8_t* array, long size);
@@ -53,44 +49,23 @@ static const char * HELP_MESSAGE = " The usage encrypt -k keyfile.bin -n 1 < pla
 
 int main(int argc, char **argv) {
 
-    int c,i ;
+    int i ;
     char key_file_name[FILE_NAME_SIZE];
     uint8_t *key ;
-    bool verbose=false;
-	uint32_t num_of_threads =0;
-	uint32_t key_length =0;
 	FILE *key_file;
 	data_buf_t *data = NULL;
-	pthread_t *thread_ids;
-	uint32_t no_of_bytes_read =0;
-	bool input_done =false;
+	pthread_t *thread_ids =NULL;
+	uint32_t no_of_bytes_read =0,num_of_threads=0;
+	uint32_t MAX_QUEUE_SIZE, MAX_BUFFERS;
+    uint32_t key_length;
+    uint32_t current_token_id =0 ,last_assigned_token_id =0;
+    bool verbose=false;
+    bool input_done =false;
 	bool found_free_buf =false;
 
 #if 0
-    while ((c = getopt(argc, argv, "k:N:d")) != EOF) {
-        switch (c) {
-            case 'N':           // Take number of threads as the input
-                num_of_threads = strtol(optarg,&temp,10);
-                break;
-            case 'k':           // key file
-                key_file = malloc(sizeof(char)*strlen(optarg));
-                strncpy(keyFile,optarg,strlen(optarg));
-                break;
-            case 'h':           // print the help message
-                printf("%s",HELP_MESSAGE);
-                break;
-            case 'v' :
-                verbose =true;
-                break;
-            default:
-                printf("Invalid argument \n ");
-                printf("%s",HELP_MESSAGE);
-        }
-    }
-#endif
-
     for(i = 1 ; i < argc ; i++)
-   {
+    {
       if(strncmp(argv[i] ,"-k", sizeof("-k")) == 0)
       {
 	      strncpy(key_file_name,argv[i+1],sizeof(argv[i+1]));
@@ -107,7 +82,9 @@ int main(int argc, char **argv) {
 		 return -1;
       }		 
    }
+#endif
 
+  
 
     if(verbose){
         printf("verbose output is on\n");
@@ -143,8 +120,12 @@ int main(int argc, char **argv) {
 	data->correspond_key = (uint8_t*)malloc(sizeof(uint8_t)*key_length);
     arr_pointers[i] = data ;
   }
-    
- 
+
+  if (pthread_mutex_init(&lock, NULL) != 0) 
+  { 
+        fprintf(stderr,"\n mutex init has failed\n"); 
+        return -1; 
+   }
   thread_ids = (pthread_t *)malloc(num_of_threads *sizeof(pthread_t));
   for(i=0 ; i< num_of_threads ; i++)
   {  
@@ -193,7 +174,7 @@ int main(int argc, char **argv) {
 	          (data->token_id == current_token_id))
           {
 		     current_token_id ++;
-		     fprintf(stdout,(char*)data->buffer);
+		     fprintf(stdout,"%*s",data->buffer_size,data->buffer);
              data->is_free = true;
 		
           }
@@ -234,20 +215,21 @@ void* thread_function(void * unused)
 	
     while(1)
     {
-	/* mutex lock */
+	   pthread_mutex_lock(&lock);
 	    if(!(input_queue.empty()) )
 	    {
 	       data =  input_queue.front();
 	       input_queue.pop();
 	       is_data_available = true;
        }
-	/* mutex unlock */
-	            		
-	   getXorOutput(data->buffer, data->correspond_key, data->buffer_size);
-	   data->is_encrypt =true;
-	   is_data_available = false;
+	  pthread_mutex_unlock(&lock); 
+	   if(is_data_available == true)
+	   {        		
+	      getXorOutput(data->buffer, data->correspond_key, data->buffer_size);
+	      data->is_encrypt =true;
+	      is_data_available = false;
+	   }
     }
-    return NULL;
 }      
 
 void getXorOutput(uint8_t* buffer1, uint8_t* buffer2, uint32_t size){
